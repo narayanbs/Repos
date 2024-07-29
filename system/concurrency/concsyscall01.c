26a7e972bb0ba51919906eaad63315627f9306e8
Narayan <narayanbala@gmail.com>
2024-07-29 18:30:45 +0530

    Updated concsyscall01.c

--- system/concurrency/concsyscall01.c
+++ system/concurrency/concsyscall01.c
@@ -1,5 +1,18 @@
-// We have to define the _GNU_SOURCE to get access to clone(2) and the CLONE_*
+/* Here, we’re using clone() in two ways, once with the CLONE_VM flag and once without.
+ We’re passing a buffer into the child process, and the child process writes a string to it.
+ We then allocate a stack size for the child process and create a function that checks whether
+ we’re executing the file using the CLONE_VM (vm) option. Furthermore, we’re creating a buffer
+ of 100 bytes in the parent process and copying a string to it, then executing
+ the clone() system call and checking for errors.
+ When we execute it without the vm argument, the CLONE_VM flag isn’t active,
+ and the parent process virtual memory is cloned into the child process.
+ The child process can access the message passed by the parent process in buffer,
+ but anything written into buffer by the child isn’t accessible to the parent process.
+ But, when we pass in the vm argument, CLONE_VM is active and the child process shares
+ the parent’s process memory. We can see it writing into buffer
+*/
 
+//  We have to define the _GNU_SOURCE to get access to clone(2) and the CLONE_*
 #define _GNU_SOURCE
 #include <sched.h>
 #include <stdio.h>
@@ -34,15 +47,13 @@ int main(int argc, char** argv) {
   int child_pid;
   char buffer[100];
   strcpy(buffer, "hello from parent");
-  if ((child_pid = clone(child_func, stack + STACK_SIZE, flags | SIGCHLD,
-                         buffer)) == -1) {
+  if ((child_pid = clone(child_func, stack + STACK_SIZE, flags | SIGCHLD, buffer)) == -1) {
     perror("clone");
     exit(1);
   }
 
   // Wait for the child process to finish
-  printf("Hello from parent process! PID: %d, Child PID: %d\n", getpid(),
-         child_pid);
+  printf("Hello from parent process! PID: %d, Child PID: %d\n", getpid(), child_pid);
   int status = waitpid(child_pid, NULL, 0);
   if (status == -1) {
     perror("wait");
