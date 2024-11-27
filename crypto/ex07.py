7592ef53577da9403acdd46cd0eb8c68b106fd43
Narayan <narayanbala@gmail.com>
2024-11-27 15:09:54 +0530

    Updated AES example

--- crypto/ex07.py
+++ crypto/ex07.py
@@ -1,37 +1,101 @@
-# RSA 
 #
-# pip install pycryptodome
+# AES Encryption/Decryption of files
 #
+import argparse
+import os
+import random
+import struct
+
+from Crypto.Cipher import AES
 
 
-# generate the RSA keys (1024-bit) and print them on the console (as hex numbers and in the PKCS#8 PEM ASN.1 format):
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
-import binascii
+def encrypt_file(key, in_filename, out_filename=None, chunksize=64 * 1024):
+    """Encrypts a file using AES (CBC mode) with the
+    given key.
 
-keyPair = RSA.generate(3072)
+    key:
+        The encryption key - a string that must be
+        either 16, 24 or 32 bytes long. Longer keys
+        are more secure.
 
-pubKey = keyPair.publickey()
-print(f"Public key:  (n={hex(pubKey.n)}, e={hex(pubKey.e)})")
-pubKeyPEM = pubKey.exportKey()
-print(pubKeyPEM.decode('ascii'))
+    in_filename:
+        Name of the input file
 
-print(f"Private key: (n={hex(pubKey.n)}, d={hex(keyPair.d)})")
-privKeyPEM = keyPair.exportKey()
-print(privKeyPEM.decode('ascii'))
+    out_filename:
+        If None, '<in_filename>.enc' will be used.
 
-# RSA Encryption
-# Next, encrypt the message using RSA-OAEP encryption scheme (RSA with PKCS#1 OAEP padding) with the RSA public key:
-msg = b'A message for encryption'
-encryptor = PKCS1_OAEP.new(pubKey)
-encrypted = encryptor.encrypt(msg)
-print("Encrypted:", binascii.hexlify(encrypted))
+    chunksize:
+        Sets the size of the chunk which the function
+        uses to read and encrypt the file. Larger chunk
+        sizes can be faster for some files and machines.
+        chunksize must be divisible by 16.
+    """
+    if not out_filename:
+        out_filename = in_filename + ".enc"
 
-# RSA Decryption
-# decrypt the message using using RSA-OAEP with the RSA private key:
-decryptor = PKCS1_OAEP.new(keyPair)
-decrypted = decryptor.decrypt(encrypted)
-print('Decrypted:', decrypted)
+    iv = os.urandom(16)
+    encryptor = AES.new(key, AES.MODE_CBC, iv)
+    filesize = os.path.getsize(in_filename)
+
+    with open(in_filename, "rb") as infile:
+        with open(out_filename, "wb") as outfile:
+            outfile.write(struct.pack("<Q", filesize))
+            outfile.write(iv)
+
+            while True:
+                chunk = infile.read(chunksize)
+                if len(chunk) == 0:
+                    break
+                elif len(chunk) % 16 != 0:
+                    chunk += b" " * (16 - len(chunk) % 16)
+
+                outfile.write(encryptor.encrypt(chunk))
+    return out_filename
 
 
+def decrypt_file(key, in_filename, out_filename=None, chunksize=24 * 1024):
+    """Decrypts a file using AES (CBC mode) with the
+    given key. Parameters are similar to encrypt_file,
+    with one difference: out_filename, if not supplied
+    will be in_filename without its last extension
+    (i.e. if in_filename is 'aaa.zip.enc' then
+    out_filename will be 'aaa.zip')
+    """
+    if not out_filename:
+        out_filename = os.path.splitext(in_filename)[0]
 
+    with open(in_filename, "rb") as infile:
+        origsize = struct.unpack("<Q", infile.read(struct.calcsize("Q")))[0]
+        iv = infile.read(16)
+        decryptor = AES.new(key, AES.MODE_CBC, iv)
+
+        with open(out_filename, "wb") as outfile:
+            while True:
+                chunk = infile.read(chunksize)
+                if len(chunk) == 0:
+                    break
+                outfile.write(decryptor.decrypt(chunk))
+
+            outfile.truncate(origsize)
+    return out_filename
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser(description="Encrypt/Decrypt AES")
+    argparser.add_argument("filename", nargs=1)
+    argparser.add_argument("-e", dest="encrypt", action="store_true")
+    argparser.add_argument("-d", dest="decrypt", action="store_true")
+    args = argparser.parse_args()
+    infile = args.filename[0]
+
+    key = b"1" * 32
+
+    if args.encrypt:
+        ofname = encrypt_file(key, infile, out_filename=infile + ".enc")
+        print("Encrypted to", ofname)
+    elif args.decrypt:
+        ofname = decrypt_file(key, infile, out_filename=infile + ".dec")
+        print("Decrypted to", ofname)
+    else:
+        argparser.print_help()
+        os._exit(1)
--- crypto/ex08.py
+++ crypto/ex08.py
@@ -1,148 +1,37 @@
-# ECC - Elliptic Curve Cryptography
+# RSA 
 #
-# pip install tinyec
-#
-
-# ECDH key exchange (Elliptic Curve Diffie–Hellman Key Exchange)
-# generate two public-private key pairs, exchange the public keys and calculate the shared secret:
-
-from tinyec import registry
-import secrets
-
-def compress(pubKey):
-    return hex(pubKey.x) + hex(pubKey.y % 2)[2:]
-
-curve = registry.get_curve('brainpoolP256r1')
-
-alicePrivKey = secrets.randbelow(curve.field.n)
-alicePubKey = alicePrivKey * curve.g
-print("Alice public key:", compress(alicePubKey))
-
-bobPrivKey = secrets.randbelow(curve.field.n)
-bobPubKey = bobPrivKey * curve.g
-print("Bob public key:", compress(bobPubKey))
-
-print("Now exchange the public keys (e.g. through Internet)")
-
-aliceSharedKey = alicePrivKey * bobPubKey
-print("Alice shared key:", compress(aliceSharedKey))
-
-bobSharedKey = bobPrivKey * alicePubKey
-print("Bob shared key:", compress(bobSharedKey))
-
-print("Equal shared keys:", aliceSharedKey == bobSharedKey)
-
-
-# ECC-Based Secret Key Derivation
-
-from tinyec import registry
-import secrets
-
-curve = registry.get_curve('brainpoolP256r1')
-
-def compress_point(point):
-    return hex(point.x) + hex(point.y % 2)[2:]
-
-def ecc_calc_encryption_keys(pubKey):
-    ciphertextPrivKey = secrets.randbelow(curve.field.n)
-    ciphertextPubKey = ciphertextPrivKey * curve.g
-    sharedECCKey = pubKey * ciphertextPrivKey
-    return (sharedECCKey, ciphertextPubKey)
-
-def ecc_calc_decryption_key(privKey, ciphertextPubKey):
-    sharedECCKey = ciphertextPubKey * privKey
-    return sharedECCKey
-
-privKey = secrets.randbelow(curve.field.n)
-pubKey = privKey * curve.g
-print("private key:", hex(privKey))
-print("public key:", compress_point(pubKey))
-
-(encryptKey, ciphertextPubKey) = ecc_calc_encryption_keys(pubKey)
-print("ciphertext pubKey:", compress_point(ciphertextPubKey))
-print("encryption key:", compress_point(encryptKey))
-
-decryptKey = ecc_calc_decryption_key(privKey, ciphertextPubKey)
-print("decryption key:", compress_point(decryptKey))
-
-#  ECC + AES hybrid encryption
-# pip install tinyec
 # pip install pycryptodome
-
-from tinyec import registry
-from Crypto.Cipher import AES
-import hashlib, secrets, binascii
-
-def encrypt_AES_GCM(msg, secretKey):
-    aesCipher = AES.new(secretKey, AES.MODE_GCM)
-    ciphertext, authTag = aesCipher.encrypt_and_digest(msg)
-    return (ciphertext, aesCipher.nonce, authTag)
-
-def decrypt_AES_GCM(ciphertext, nonce, authTag, secretKey):
-    aesCipher = AES.new(secretKey, AES.MODE_GCM, nonce)
-    plaintext = aesCipher.decrypt_and_verify(ciphertext, authTag)
-    return plaintext
-
-def ecc_point_to_256_bit_key(point):
-    sha = hashlib.sha256(int.to_bytes(point.x, 32, 'big'))
-    sha.update(int.to_bytes(point.y, 32, 'big'))
-    return sha.digest()
-
-curve = registry.get_curve('brainpoolP256r1')
-
-def encrypt_ECC(msg, pubKey):
-    ciphertextPrivKey = secrets.randbelow(curve.field.n)
-    sharedECCKey = ciphertextPrivKey * pubKey
-    secretKey = ecc_point_to_256_bit_key(sharedECCKey)
-    ciphertext, nonce, authTag = encrypt_AES_GCM(msg, secretKey)
-    ciphertextPubKey = ciphertextPrivKey * curve.g
-    return (ciphertext, nonce, authTag, ciphertextPubKey)
-
-def decrypt_ECC(encryptedMsg, privKey):
-    (ciphertext, nonce, authTag, ciphertextPubKey) = encryptedMsg
-    sharedECCKey = privKey * ciphertextPubKey
-    secretKey = ecc_point_to_256_bit_key(sharedECCKey)
-    plaintext = decrypt_AES_GCM(ciphertext, nonce, authTag, secretKey)
-    return plaintext
-
-msg = b'Text to be encrypted by ECC public key and ' \
-      b'decrypted by its corresponding ECC private key'
-print("original msg:", msg)
-privKey = secrets.randbelow(curve.field.n)
-pubKey = privKey * curve.g
-
-encryptedMsg = encrypt_ECC(msg, pubKey)
-encryptedMsgObj = {
-    'ciphertext': binascii.hexlify(encryptedMsg[0]),
-    'nonce': binascii.hexlify(encryptedMsg[1]),
-    'authTag': binascii.hexlify(encryptedMsg[2]),
-    'ciphertextPubKey': hex(encryptedMsg[3].x) + hex(encryptedMsg[3].y % 2)[2:]
-}
-print("encrypted msg:", encryptedMsgObj)
-
-decryptedMsg = decrypt_ECC(encryptedMsg, privKey)
-print("decrypted msg:", decryptedMsg)
+#
 
 
-# ECIES encryption scheme
-# pip install eciespy
-
-from ecies.utils import generate_eth_key
-from ecies import encrypt, decrypt
+# generate the RSA keys (1024-bit) and print them on the console (as hex numbers and in the PKCS#8 PEM ASN.1 format):
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 import binascii
 
-privKey = generate_eth_key()
-privKeyHex = privKey.to_hex()
-pubKeyHex = privKey.public_key.to_hex()
-print("Encryption public key:", pubKeyHex)
-print("Decryption private key:", privKeyHex)
+keyPair = RSA.generate(3072)
 
-plaintext = b'Some plaintext for encryption'
-print("Plaintext:", plaintext)
+pubKey = keyPair.publickey()
+print(f"Public key:  (n={hex(pubKey.n)}, e={hex(pubKey.e)})")
+pubKeyPEM = pubKey.exportKey()
+print(pubKeyPEM.decode('ascii'))
 
-encrypted = encrypt(pubKeyHex, plaintext)
+print(f"Private key: (n={hex(pubKey.n)}, d={hex(keyPair.d)})")
+privKeyPEM = keyPair.exportKey()
+print(privKeyPEM.decode('ascii'))
+
+# RSA Encryption
+# Next, encrypt the message using RSA-OAEP encryption scheme (RSA with PKCS#1 OAEP padding) with the RSA public key:
+msg = b'A message for encryption'
+encryptor = PKCS1_OAEP.new(pubKey)
+encrypted = encryptor.encrypt(msg)
 print("Encrypted:", binascii.hexlify(encrypted))
 
-decrypted = decrypt(privKeyHex, encrypted)
-print("Decrypted:", decrypted)
+# RSA Decryption
+# decrypt the message using using RSA-OAEP with the RSA private key:
+decryptor = PKCS1_OAEP.new(keyPair)
+decrypted = decryptor.decrypt(encrypted)
+print('Decrypted:', decrypted)
+
+
 
