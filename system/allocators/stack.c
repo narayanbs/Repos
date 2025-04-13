8318a605c5333357833be3ef6cc0a6e5c4cf62af
Narayan <narayanbala@gmail.com>
2025-04-13 08:17:44 +0530

    Updated stack.c in allocators

--- system/allocators/stack.c
+++ system/allocators/stack.c
@@ -1,198 +1,79 @@
-#include <assert.h>
-#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 
-#ifndef DEFAULT_ALIGNMENT
-#define DEFAULT_ALIGNMENT (2 * sizeof(void *))
-#endif
+#define ALIGNMENT (sizeof(void *))
 
-typedef struct Stack Stack;
-
-struct Stack {
+typedef struct Stack {
   unsigned char *buf;
   size_t buf_len;
-  size_t offset;
-};
+  size_t prev_offset;
+  size_t curr_offset;
+} Stack;
 
-typedef struct Stack_Allocation_Header Stack_Allocation_Header;
-
-struct Stack_Allocation_Header {
+typedef struct StackHeader {
+  size_t prev_offset;
   uint8_t padding;
-};
+} StackHeader;
 
-bool is_power_of_two(size_t x) {
-  return (x & (x - 1)) == 0;
+void stack_init(Stack *s, void *buf, size_t buf_len) {
+  s->buf = (unsigned char *)buf;
+  s->buf_len = buf_len;
+  s->prev_offset = 0;
+  s->curr_offset = 0;
 }
 
-void stack_init(Stack *s, void *backing_buffer, size_t backing_buffer_length) {
-  s->buf = (unsigned char *)backing_buffer;
-  s->buf_len = backing_buffer_length;
-  s->offset = 0;
-}
-
-size_t calc_padding_with_header(uintptr_t ptr, uintptr_t alignment, uintptr_t header_size) {
-  uintptr_t p, a, padding, modulo, needed_space;
-
-  assert(is_power_of_two(alignment));
-
-  p = ptr;
-  a = alignment;
-
-  modulo = p & (a - 1);
-
-  padding = 0;
-  needed_space = 0;
-
-  if (modulo != 0) {
-    padding = a - modulo;
-  }
-  needed_space = (uintptr_t)header_size;
-
-  if (padding < needed_space) {
-    needed_space -= padding;
-    if ((needed_space & (a - 1)) != 0) {
-      padding += a * (1 + (needed_space / a));
+uintptr_t calc_size_with_header(uintptr_t ptr, size_t alignment, size_t header_size) {
+  uintptr_t align_ptr = (ptr + (alignment - 1)) & ~(alignment - 1);
+  uintptr_t padding = align_ptr - ptr;
+  if (padding < header_size) {
+    uintptr_t nested_header = header_size - padding;
+    if ((nested_header & (alignment - 1)) != 0) {
+      padding += alignment * (1 + (nested_header) / (alignment));
     } else {
-      padding += a * (needed_space / a);
+      padding += alignment * (nested_header / alignment);
     }
   }
-  return (size_t)padding;
-}
-
-void *stack_align_alloc(Stack *s, size_t size, size_t alignment) {
-  uintptr_t curr_addr, next_addr;
-  size_t padding;
-  Stack_Allocation_Header *header;
-
-  assert(is_power_of_two(alignment));
-
-  if (alignment > 128) {
-    alignment = 128;
-  }
-
-  curr_addr = (uintptr_t)s->buf + (uintptr_t)s->offset;
-  padding = calc_padding_with_header(curr_addr, (uintptr_t)alignment, sizeof(Stack_Allocation_Header));
-  if (s->offset + padding + size > s->buf_len) {
-    return NULL;
-  }
-  s->offset += padding;
-
-  next_addr = curr_addr + (uintptr_t)padding;
-  header = (Stack_Allocation_Header *)(next_addr - sizeof(Stack_Allocation_Header));
-  header->padding = (uint8_t)padding;
-
-  s->offset += size;
-  return memset((void *)next_addr, 0, size);
+  return padding;
 }
 
 void *stack_alloc(Stack *s, size_t size) {
-  return stack_align_alloc(s, size, DEFAULT_ALIGNMENT);
-}
-
-void stack_free(Stack *s, void *ptr) {
-  if (ptr != NULL) {
-    uintptr_t start, end, curr_addr;
-    Stack_Allocation_Header *header;
-    size_t prev_offset;
-
-    start = (uintptr_t)s->buf;
-    end = start + (uintptr_t)s->buf_len;
-    curr_addr = (uintptr_t)ptr;
-
-    if (!(start <= curr_addr && curr_addr < end)) {
-      assert(0 && "Out of bounds memory address passed to stack allocator (free)");
-      return;
-    }
-
-    if (curr_addr >= start + (uintptr_t)s->offset) {
-      return;
-    }
-
-    header = (Stack_Allocation_Header *)(curr_addr - sizeof(Stack_Allocation_Header));
-    prev_offset = (size_t)(curr_addr - (uintptr_t)header->padding - start);
-    s->offset = prev_offset;
-  }
-}
-
-void *stack_resize_align(Stack *s, void *ptr, size_t old_size, size_t new_size, size_t alignment) {
-  if (ptr == NULL) {
-    return stack_align_alloc(s, new_size, alignment);
-  } else if (new_size == 0) {
-    stack_free(s, ptr);
-    return NULL;
-  } else {
-    uintptr_t start, end, curr_addr;
-    size_t min_size = old_size < new_size ? old_size : new_size;
-    void *new_ptr;
-
-    start = (uintptr_t)s->buf;
-    end = start + (uintptr_t)s->buf_len;
-    curr_addr = (uintptr_t)ptr;
-    if (!(start <= curr_addr && curr_addr < end)) {
-      assert(0 && "Out of bounds memory address passed to stack allocator (resize)");
-      return NULL;
-    }
-
-    if (curr_addr >= start + (uintptr_t)s->offset) {
-      // Treat as a double free
-      return NULL;
-    }
-
-    if (old_size == new_size) {
-      return ptr;
-    }
-
-    new_ptr = stack_align_alloc(s, new_size, alignment);
-    memmove(new_ptr, ptr, min_size);
+  uintptr_t curr_ptr = (uintptr_t)s->buf + (uintptr_t)s->curr_offset;
+  uintptr_t padding = calc_size_with_header(curr_ptr, ALIGNMENT, sizeof(StackHeader));
+  uintptr_t offset = padding + s->curr_offset;
+  if (offset + size < s->buf_len) {
+    void *new_ptr = &s->buf[offset];
+    memset(new_ptr, 0, size);
+    StackHeader *sh = (StackHeader *)((char *)new_ptr - sizeof(StackHeader));
+    sh->padding = padding;
+    sh->prev_offset = s->prev_offset;
+    s->curr_offset = offset + size;
     return new_ptr;
   }
+  return NULL;
 }
 
-void *stack_resize(Stack *s, void *ptr, size_t old_size, size_t new_size) {
-  return stack_resize_align(s, ptr, old_size, new_size, DEFAULT_ALIGNMENT);
-}
-
-void stack_free_all(Stack *s) {
-  s->offset = 0;
+void *stack_free(Stack *s, void *ptr) {
+  StackHeader *sh = (StackHeader *)((char *)ptr - sizeof(StackHeader));
+  s->prev_offset = sh->prev_offset;
+  s->curr_offset = (uintptr_t)ptr - sh->padding - (uintptr_t)s->buf;
+  return &s->buf[s->prev_offset];
 }
 
 int main(void) {
-  unsigned char backing_buffer[256];
+  char buf[256];
   Stack s = {0};
-  stack_init(&s, backing_buffer, 256);
-
-  int *x;
+  stack_init(&s, buf, sizeof(buf));
+  int *i;
   float *f;
-  char *str;
-
-  // Reset all stack offsets for each loop
-  stack_free_all(&s);
-
-  x = (int *)stack_alloc(&s, sizeof(int));
-  *x = 123;
-
+  i = (int *)stack_alloc(&s, sizeof(int));
   f = (float *)stack_alloc(&s, sizeof(float));
-  *f = 987;
-
-  str = stack_alloc(&s, 10);
-  memmove(str, "Hellope", 7);
-  printf("%p: %s\n", str, str);
-
-  str = stack_resize(&s, str, 10, 16);
-  memmove(str + 7, " world!", 7);
-  printf("%p: %s\n", str, str);
-  stack_free(&s, str);
-
-  printf("%p: %f\n", f, *f);
+  *i = 20;
+  *f = 54.234;
+  printf("%d : %p\n", *i, i);
+  printf("%f : %p\n", *f, f);
   stack_free(&s, f);
-
-  printf("%p: %d\n", x, *x);
-  stack_free(&s, x);
-
-  stack_free_all(&s);
-
+  printf("%d : %p\n", *i, i);
   return 0;
 }
