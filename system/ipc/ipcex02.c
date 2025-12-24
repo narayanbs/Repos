48348f67e9bf57c7cc6137aa1481113bac4e1a45
Narayan <narayanbala@gmail.com>
2025-12-24 12:53:07 +0530

    Updated signal ipc example

--- system/ipc/ipcex02.c
+++ system/ipc/ipcex02.c
@@ -5,13 +5,16 @@
 #include <unistd.h>
 
 volatile sig_atomic_t got_usr1;
+int counter = 0;
 
-void sigusr1_handler(int sig) { got_usr1 = 1; }
+void sigusr1_handler(int sig) {
+  got_usr1 = 1;
+}
 
 int main(void) {
   struct sigaction sa = {
       .sa_handler = sigusr1_handler,
-      .sa_flags = 0, // or SA_RESTART
+      .sa_flags = 0,  // or SA_RESTART
       .sa_mask = 0,
   };
 
@@ -21,6 +24,23 @@ int main(void) {
     perror("sigaction");
     exit(1);
   }
+
+  printf("PID %d: ...\n", getpid());
+  sigset_t set, old;
+
+  /* Block SIGUSR1 */
+  sigemptyset(&set);
+  sigaddset(&set, SIGUSR1);
+  sigprocmask(SIG_BLOCK, &set, &old);
+
+  //------------critical section ----
+  counter++;
+  printf("counter = %d\n", counter);
+  // doing some heavy work while we receive the signal
+  sleep(15);
+
+  /* Restore previous mask */
+  sigprocmask(SIG_SETMASK, &old, NULL);
 
   while (!got_usr1) {
     printf("PID %d: working hard...\n", getpid());
